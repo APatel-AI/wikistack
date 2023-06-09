5c18fa1a6df22e98c206e578457ea297009ef8f8
DanaPaolone <dpaolone13@gmail.com>
2023-06-09 11:32:52 -0400

    starting Tab 2

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
+const { db } = require("./models");
+const { db, Page, User } = require("./models");
 
-
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.get('/', (req, res, next) => {
-  
-    const layout = (content) =>`
+app.get("/", (req, res, next) => {
+  const layout = (content) => `
       <!DOCTYPE html>
       <html>
         <head>
@@ -23,9 +22,25 @@ app.get('/', (req, res, next) => {
         </body>
       </html>
     `;
-  
-    res.send(layout('Hello, world!'));
-  });
-  
 
-app.listen(3000, 'localhost') ;
+  res.send(layout("Hello, world!"));
+});
+
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
+
+const PORT = 3000;
+
+const init = async () => {
+  await Page.sync();
+  await User.sync();
+  // make sure that you have a PORT constant
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}!`);
+  });
+};
+
+init();
+
+//app.listen(3000, "localhost");
